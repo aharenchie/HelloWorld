e1a3ae87bf7026781dd33c29770f49329893a863
aharenchie <e125731@ie.u-ryukyu.ac.jp>
2014-10-25 10:35:55 +0900

    first commite

---
+++ HelloWorld/RootViewContorller.swift
@@ -0,0 +1,13 @@
+//
+//  RootViewContorller.swift
+//  HelloWorld
+//
+//  Created by Chie AHAREN on 2014/10/25.
+//  Copyright (c) 2014年 Chie AHAREN. All rights reserved.
+//
+
+import Cocoa
+
+class RootViewContorller: UIViewController {
+
+}
